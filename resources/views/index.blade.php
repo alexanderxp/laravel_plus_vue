6454b41d138908acdaf6e7ee3bc2633af5e4cec5
Alexander <alexanderxp@ukr.net>
2021-10-07 18:27:11 +0300

    add index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="vewport"
+            context="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compartible" content="ie=edge">
+        <title>Blogger</title>
+    </head>    
+    <body>
+    </body>
+</html>
