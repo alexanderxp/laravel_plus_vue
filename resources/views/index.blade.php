1d51a13415b575b480ea1e90d44eca6a85096dee
Alexander <alexanderxp@ukr.net>
2021-10-08 16:18:38 +0300

    add <test></test> to index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,7 @@
     </head>    
     <body>
         <div id="app">
+            <test></test>
             <example-component></example-component>
         </div>
         <script src="./js/app.js"></script>
