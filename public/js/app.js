b0ce1eaa5b8d8e2f6f46f9df3bc9ae10a3ed3740
Alexander <alexanderxp@ukr.net>
2021-10-10 23:02:17 +0300

    del Vue.component('test', require('./components/test.vue').default); from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2077,9 +2077,8 @@ window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm.js"
  */
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-Vue.component('example-component', Object(function webpackMissingModule() { var e = new Error("Cannot find module './components/ExampleComponent.vue'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
-Vue.component('test', Object(function webpackMissingModule() { var e = new Error("Cannot find module './components/test.vue'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
