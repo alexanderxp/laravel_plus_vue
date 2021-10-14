af28d429541bc17f6623ed498c00a1b4d24325aa
Alexander <alexanderxp@ukr.net>
2021-10-14 23:36:25 +0300

    del <li><a href="#">Parent</a><div class="uk-navbar-dropdown">... & change line <li><a href="#">Active</a></li>

--- public/js/app.js
+++ public/js/app.js
@@ -2080,16 +2080,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   name: "Header"
 });
@@ -37663,29 +37653,7 @@ var staticRenderFns = [
       [
         _c("div", { staticClass: "uk-navbar-center" }, [
           _c("ul", { staticClass: "uk-navbar-nav" }, [
-            _c("li", { staticClass: "uk-active" }, [
-              _c("a", { attrs: { href: "#" } }, [_vm._v("Active")])
-            ]),
-            _vm._v(" "),
-            _c("li", [
-              _c("a", { attrs: { href: "#" } }, [_vm._v("Parent")]),
-              _vm._v(" "),
-              _c("div", { staticClass: "uk-navbar-dropdown" }, [
-                _c("ul", { staticClass: "uk-nav uk-navbar-dropdown-nav" }, [
-                  _c("li", { staticClass: "uk-active" }, [
-                    _c("a", { attrs: { href: "#" } }, [_vm._v("Active")])
-                  ]),
-                  _vm._v(" "),
-                  _c("li", [
-                    _c("a", { attrs: { href: "#" } }, [_vm._v("Item")])
-                  ]),
-                  _vm._v(" "),
-                  _c("li", [
-                    _c("a", { attrs: { href: "#" } }, [_vm._v("Item")])
-                  ])
-                ])
-              ])
-            ]),
+            _c("li", [_c("a", { attrs: { href: "#" } }, [_vm._v("Active")])]),
             _vm._v(" "),
             _c("li", [_c("a", { attrs: { href: "#" } }, [_vm._v("Item")])])
           ])
