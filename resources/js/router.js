661f36d61e129c9442e06ad5e29e82d86fc868a7
Alexander <alexanderxp@ukr.net>
2021-10-15 02:12:20 +0300

    add import Index from "./views/Index";  &  const routes = [ { path: '/', component: Index  } ];  to router.js

--- resources/js/router.js
+++ resources/js/router.js
@@ -2,3 +2,12 @@ import vueRouter from 'vue-router';
 import Vue from 'vue';
 
 Vue.use(vueRouter);
+
+import Index from "./views/Index";
+
+const routes = [
+    { 
+        path: '/', 
+        component: Index  
+    }
+];
