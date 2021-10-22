dbee16835491b31ed92ccaf72e5e44331ecfd7f0
Alexander <alexanderxp@ukr.net>
2021-10-22 13:37:28 +0300

    change <h1>home page</h1> to <h1>Добро пожаловать!</h1> in Index.vue

--- public/js/app.js
+++ public/js/app.js
@@ -37971,7 +37971,7 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", [_c("h1", [_vm._v("home page")])])
+    return _c("div", [_c("h1", [_vm._v("Добро пожаловать!")])])
   }
 ]
 render._withStripped = true
