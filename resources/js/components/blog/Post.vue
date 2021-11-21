9023f0223efe1d440b57d7b7b2f5b63f60588a7d
Alexander <alexanderxp@ukr.net>
2021-11-21 19:41:30 +0200

    change {{ post.title }}{{ post.created_at }}{{ post.body }} to {{ title }}{{ date }}{{ body }} in Post.vue

--- resources/js/components/blog/Post.vue
+++ resources/js/components/blog/Post.vue
@@ -3,15 +3,15 @@
         <div class="uk-card-header">
             <div class="uk-grid-small uk-flex-middle" uk-grid>
                 <div class="uk-width-expand">
-                    <h3 class="uk-card-title uk-margin-remove-bottom">{{ post.title }}</h3>
+                    <h3 class="uk-card-title uk-margin-remove-bottom">{{ title }}</h3>
                     <p class="uk-text-meta uk-margin-remove-top">
-                        <time datetime="2016-04-01T19:00">{{ post.created_at }}</time>
+                        <time datetime="2016-04-01T19:00">{{ date }}</time>
                     </p>
                 </div>
             </div>
         </div>
         <div class="uk-card-body">
-            <p>{{ post.body }}</p>
+            <p>{{ body }}</p>
         </div>
         <div class="uk-card-footer">
             <a href="#" class="uk-button uk-button-text">Read more</a>
@@ -35,7 +35,7 @@
             date: {
                 type: String,
                 default: "NULL"
-            },
+            }
         }
     }
 
