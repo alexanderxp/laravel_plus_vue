29f9d5dd9466e90d51353e4a94c6ebd976c7309a
Alexander <alexanderxp@ukr.net>
2021-11-21 19:23:36 +0200

    dell tags from file Blog.vue & add its tags to file Post.vue

--- resources/js/components/blog/Post.vue
+++ resources/js/components/blog/Post.vue
@@ -1,6 +1,22 @@
 <template>
-
-
+    <div class="uk-card uk-card-default uk-width-1-2@m" v-for="post in posts">
+        <div class="uk-card-header">
+            <div class="uk-grid-small uk-flex-middle" uk-grid>
+                <div class="uk-width-expand">
+                    <h3 class="uk-card-title uk-margin-remove-bottom">{{ post.title }}</h3>
+                    <p class="uk-text-meta uk-margin-remove-top">
+                        <time datetime="2016-04-01T19:00">{{ post.created_at }}</time>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="uk-card-body">
+            <p>{{ post.body }}</p>
+        </div>
+        <div class="uk-card-footer">
+            <a href="#" class="uk-button uk-button-text">Read more</a>
+        </div>
+    </div>
 </template>
 
 
--- resources/js/views/Blog.vue
+++ resources/js/views/Blog.vue
@@ -2,25 +2,7 @@
     <div>   
         <spin v-if="loading"></spin>
         <div style="display:flex; flex-wrap: wrap;" v-else>      
-            <div class="uk-card uk-card-default uk-width-1-2@m" v-for="post in posts">
-                <div class="uk-card-header">
-                    <div class="uk-grid-small uk-flex-middle" uk-grid>
-
-                        <div class="uk-width-expand">
-                            <h3 class="uk-card-title uk-margin-remove-bottom">{{ post.title }}</h3>
-                            <p class="uk-text-meta uk-margin-remove-top">
-                                <time datetime="2016-04-01T19:00">{{ post.created_at }}</time>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="uk-card-body">
-                    <p>{{ post.body }}</p>
-                </div>
-                <div class="uk-card-footer">
-                    <a href="#" class="uk-button uk-button-text">Read more</a>
-                </div>
-            </div>
+            
         </div>
     </div>
 </template>
