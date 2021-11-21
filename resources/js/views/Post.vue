daed37313bd9765157104bfb6ff69bdfe11f6231
Alexander <alexanderxp@ukr.net>
2021-11-21 21:08:41 +0200

    add <div><h1>Title <span>12.12.2020</span></h1><p class="uk-text-load">Lora Lora color , commit abstract tell, many many ingredient. Full HD</p></div> to Post.vue (another Post.vue )

--- resources/js/views/Post.vue
+++ resources/js/views/Post.vue
@@ -1,6 +1,8 @@
 <template>
-
-
+    <div>
+        <h1>Title <span>12.12.2020</span></h1>
+        <p class="uk-text-load">Lora Lora color , commit abstract tell, many many ingredient. Full HD</p>
+    </div>
 </template>
 
 
